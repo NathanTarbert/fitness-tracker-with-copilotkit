ccc1ce33b5c74bf2c576e0a50aa6296cabfcf91d
Nathan Tarbert <nathantarbert@Nathans-MBP-2.lan>
2024-08-15 17:04:51 -0400

    users are required to be logged in in order to navigate to the dashboard

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,20 +1,27 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Nav() {
   const router = useRouter();
-  const handleSignOut = (event: MouseEvent<HTMLButtonElement>) => {
-    // Clear user data from storage
-    localStorage.removeItem("userToken");
-    sessionStorage.removeItem("userSession");
+  const [userName, setUserName] = useState("");
 
-    // Redirect to the login page
+  useEffect(() => {
+    const storedUserName = localStorage.getItem("username");
+    if (storedUserName) {
+      setUserName(storedUserName);
+      console.log({ localStorage });
+    }
+  }, []);
+
+  const handleSignOut = (event: MouseEvent<HTMLButtonElement>) => {
+    localStorage.removeItem("userToken");
+    localStorage.removeItem("username");
     router.push("/login");
   };
 
   return (
     <nav className="px-4 py-3 bg-blue-50 h-[10vh] flex items-center justify-between">
-      <h3 className="font-bold text-xl">Hi-Fi Inc.</h3>
+      <h3 className="font-bold text-xl">Hi-Fi Inc. - Welcome {userName}</h3>
       <button
         className="px-4 py-2 bg-red-500 text-red-50 rounded-md"
         onClick={handleSignOut}>
